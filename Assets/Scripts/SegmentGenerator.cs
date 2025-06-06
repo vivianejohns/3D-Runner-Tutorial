ecd81d44ddae388522ef30c70614a07d285e20bb
Viviane Johns <75577574+vivianejohns@users.noreply.github.com>
2025-06-07 01:03:23 +0200

    make segments out of scene dissapear

--- Assets/Scripts/SegmentGenerator.cs
+++ Assets/Scripts/SegmentGenerator.cs
@@ -1,13 +1,21 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SegmentGenerator : MonoBehaviour
 {
     public GameObject[] segment;
+    public int camOffset = -25
+    ;
 
     [SerializeField] int zPos = 50;
     [SerializeField] bool creatingSegment = false;
     [SerializeField] int segmentNumber;
+    // reference camera for calculating out of sight position
+    [SerializeField] private Camera cam;
+    [SerializeField] private float destroyDelay = 1;
+    private List<GameObject> activeSegments = new List<GameObject>();
 
     void Update()
     {
@@ -16,16 +24,43 @@ public class SegmentGenerator : MonoBehaviour
             creatingSegment = true;
             StartCoroutine(SegmentGen());
         }
-        
+    }
+
+       void LateUpdate()
+    {
+        foreach (GameObject segment in activeSegments)
+        {
+            if (!IsInViewPort(segment))
+            {
+                StartCoroutine(DelayedDestroy(segment));
+            }
+        }
     }
 
     // co-routine
     IEnumerator SegmentGen()
     {
-        segmentNumber = Random.Range(0, segment.Length);
-        Instantiate(segment[segmentNumber], new Vector3(0, 0, zPos), Quaternion.identity);
+        segmentNumber = UnityEngine.Random.Range(0, segment.Length);
+        GameObject newSegment = Instantiate(segment[segmentNumber], new Vector3(0, 0, zPos), Quaternion.identity);
+        activeSegments.Add(newSegment);
         zPos += 50;
         yield return new WaitForSeconds(10);
         creatingSegment = false;
+    }
+
+    private bool IsInViewPort(GameObject segment)
+    {
+        // Prüfen ob das Segment innerhalb des Viewports ist
+        bool isInViewPort = segment.transform.position.z >= cam.transform.position.z + camOffset;
+
+        Debug.Log(isInViewPort);
+        return isInViewPort;
+    }
+    
+    private IEnumerator DelayedDestroy(GameObject segment)
+    {
+        yield return new WaitForSeconds(destroyDelay);
+        activeSegments.Remove(segment);
+        Destroy(segment);
     }
 }
